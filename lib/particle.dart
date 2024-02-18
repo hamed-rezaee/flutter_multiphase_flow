24028c05c346625a97bf0e141d7d349f3dd4c5df
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2024-02-18 11:40:29 +0100

    add particle class

---
+++ lib/particle.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Particle {
+  Particle.empty();
+
+  Particle(this.x, this.y, this.type);
+
+  double x = 0;
+  double y = 0;
+  double fx = 0;
+  double fy = 0;
+  double vx = 0;
+  double vy = 0;
+  double density = 0;
+  double densityNear = 0;
+  int gx = 0;
+  int gy = 0;
+  int type = 0;
+
+  Color getColor() => switch (type) {
+        0 => Colors.blue,
+        1 => Colors.red,
+        2 => Colors.green,
+        3 => Colors.yellow,
+        4 => Colors.purple,
+        _ => Colors.black,
+      };
+}
