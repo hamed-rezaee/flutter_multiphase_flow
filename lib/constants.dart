4ea91ba4a4bd0f4c9cb3f982fa7090a6d5e88ad1
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2024-02-18 11:40:16 +0100

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+const double width = 300;
+const double height = 300;
+
+const double interval = 15;
+
+const double gravity = 0.05;
+const double range = 15;
+const double range2 = range * range;
+const double density = 2.5;
+const double pressure = 1;
+const double pressureNear = 1;
+const double viscosity = 0.1;
+const int numGrids = 18;
+const double invGridSize = 1 / (280 / numGrids);
